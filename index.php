bab495b71dd5631f10496bd24e064ccccf06f759
lettier <contact@lettier.com>
2013-03-09 15:39:25 -0500

    Changed index.php.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Cooperative Commuting</title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+        <meta name="viewport" content="width=device-width"><!--For mobile browsers.-->
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
