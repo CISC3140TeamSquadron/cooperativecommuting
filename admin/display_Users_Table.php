0fb69aeaac52a9daa866b4d302df00b4595b84a0
Peter <clearskyopening@gmail.com>
2013-04-29 21:26:25 -0300

    Update display_Users_Table.php

--- admin/display_Users_Table.php
+++ admin/display_Users_Table.php
@@ -13,29 +13,21 @@
     <form method="post" action="edit2.php">
 
 	<?php
-		$local_Host = 'localhost';
-		$username = '';
-		$DB_Username = '';
-		$password = '';
-	
-		$conn = mysql_connect($local_Host, $username, $password) 
-		or die('Could not connect:'.mysql_error());	
-		$db_found = mysql_select_db($DB_Username);	
-	
-		if ($db_found)  {		
-			$query = "SELECT * FROM cc_users ORDER BY user_id";
-			$result = mysql_query($query) or die('Query failed: 
-			'.mysql_error());	
+		include 'connect.php'; //Connect to the MySQL database.	
 		
-			echo "<table border=4 align=center>\n";
+		$query = "SELECT * FROM cc_users ORDER BY user_id";
+		$result = mysql_query($query) or die('Query failed: '.mysql_error());	
+		
+		echo "<table border=2 align=center>\n";
 
-			echo "\t<tr><td>USER ID</td><td>EMAIL</td><td>PASSWORD</td><td>FIRSTNAME</td>
-			<td>LASTNAME</td><td>GENDER</td><td>Street</td><td>APT</td><td>CITY</td>
-			<td>STATE</td><td>ZIP</td><td>COUNTRY</td><td>COM STREET</td><td>COM CITY</td>
-			<td>COM STATE</td><td>COM ZIP</td><td>DRIVER</td><td>VEHICLE</td>
-			<td>PREFERENCE</td><td>ABOUT</td><td>REASON</td></tr>\n";
-			while ($row = mysql_fetch_assoc($result))  {
-			 //First line below passes the value of 'user_ID' into the URL link in order to update the DB.
+		echo "\t<tr><td>ID</td><td>Email</td><td>Password</td><td>F. Name</td>
+		<td>L. Name</td><td>Sex</td><td>Street</td><td>Apt</td><td>City</td>
+		<td>State</td><td>Zip</td><td>Country</td><td>Com Street</td><td>Com City</td>
+		<td>Com State</td><td>Com Zip</td><td>Driver?</td><td>Owns</td>
+		<td>Preference</td><td>About</td><td>Reason</td></tr>\n";
+			
+		while ($row = mysql_fetch_assoc($result))  {
+			//First line below passes the value of 'user_ID' into the URL link in order to update the DB.
 			 echo ("<tr><td><a href=\"edit_User_Form.php?user_ID=$row[user_id]\">User\t$row[user_id]</a></td>");
        		 echo ("<td>$row[email]</td>");
        		 echo ("<td>$row[pword]</td>");
@@ -57,16 +49,10 @@
        		 echo ("<td>$row[vehicle_pref]</td>");
        		 echo ("<td>$row[user_desc]</td>");
        		 echo ("<td>$row[commuting_for]</td>");
-			}
-			
-			echo "</table> \n";
-			
-			mysql_close($conn);	
 		}
-		else { 
-			echo "Database not found";		
-			mysql_close($conn);
-		}
+			
+		echo "</table> \n";
+		
 	?>      
       
     </form>
