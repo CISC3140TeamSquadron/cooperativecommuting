37111a82e2134f874b552eb38bf8198652db5520
Peter <clearskyopening@gmail.com>
2013-05-13 19:39:38 -0300

    Update admin_Welcome.php
    Added CSS

--- admin/admin_Welcome.php
+++ admin/admin_Welcome.php
@@ -6,12 +6,29 @@
 
   </head>
 
-  <body>  	
+  <body>		
 	
-	<style> h1 {text-align:center} </style><h1>Welcome to the 
-	Cooperative Commuting Admin Page!</h1><style> form {text-align:center} </style>
+	<style type="text/css">
+	
+	h1 {
+		text-align:center;
+		color: #666666;
+		padding: 10px; 
+	}
+	body {
+		font-family: Arial, Verdana, sans-serif;
+		background-color: #f8f8f8;
+	}
+	form {
+		text-align:center;
+	}
+	</style>
+	
+	<h1>Welcome to the Cooperative Commuting Admin Page!</h1>
+	
 	<form method="link" action="display_Users_Table.php">
-	<input type="submit" value="View Cooperative Commuting Users"></form>	
+	<input type="submit" value="View Cooperative Commuting Users">
+	</form>	
 	
   </body>
 	
