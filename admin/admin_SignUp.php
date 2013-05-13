f3192866754c8554acaf7ce9f5059621837e5634
Peter <clearskyopening@gmail.com>
2013-05-13 19:38:55 -0300

    Update admin_SignUp.php

--- admin/admin_SignUp.php
+++ admin/admin_SignUp.php
@@ -1,19 +1,20 @@
 <?php
-  /*This php file connects to the database and inserts the field data to the 
+	/*This php file connects to the database and inserts the field data to the 
 	'admins_Table' table based on the post input from 'admin_SignUp_Form'.*/
 	
 	include 'connect.php'; //Connect to the MySQL database.
 	
+	//Not necessary now since create-an-admin-account feature has been removed. 
 	//Checks if user entered empty input.
-	if( !empty($_POST['user_email']) && !empty($_POST['user_password']) && 
+	/*if( !empty($_POST['user_email']) && !empty($_POST['user_password']) && 
 		!empty($_POST['firstname']) && !empty($_POST['lastname']) &&
 		!empty($_POST['security_answer']) ) {
-
+	*/
 		$email = $_POST['user_email'];
 		
 		//If valid email block.
 		if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-					$pass = $_POST['user_password'];
+			$pass = $_POST['user_password'];
 			$firstname = $_POST['firstname'];
 			$lastname = $_POST['lastname'];
 			$security_ans = $_POST['security_answer'];
